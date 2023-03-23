887a006cdcd0bec2669c635d12b4d34a6251327d
LearnBite <frostcatlover@outlook.com>
2023-03-22 21:19:58 -0700

    Submitted 'Multiplication Table for Number'

--- 8Kyu.js
+++ 8Kyu.js
@@ -32,3 +32,34 @@ function squareSum(numbers){
   numbers.forEach(num => {sum += Math.pow(num, 2)});
   return sum;
 }
+
+
+// Multiplication Table for Number
+//Your goal is to return multiplication table for number that is always an //integer from 1 to 10.
+//
+//For example, a multiplication table (string) for number == 5 looks like below:
+//
+//1 * 5 = 5
+//2 * 5 = 10
+//3 * 5 = 15
+//4 * 5 = 20
+//5 * 5 = 25
+//6 * 5 = 30
+//7 * 5 = 35
+//8 * 5 = 40
+//9 * 5 = 45
+//10 * 5 = 50
+//P. S. You can use \n in string to jump to the next line.
+//
+//Note: newlines should be added between rows, but there should be no trailing //newline at the end. If you're unsure about the format, look at the sample tests.
+
+function multiTable(number) {
+  let multiTable = ``
+  let product;
+  for (let i = 1; i <= 9; i++){
+    product = i * number;
+    multiTable +=`${i} * ${number} = ${product}\n`
+  };
+  product = 10 * number
+  return multiTable += `10 * ${number} = ${product}`
+}
