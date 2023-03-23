d26586e9eeade5ba01c0067da1e7ddacbe080a6f
Ryan <frostcatlover@outlook.com>
2023-03-23 15:22:28 -0700

    Solved 'Are They the 'Same'

--- 6Kyu.js
+++ 6Kyu.js
@@ -34,3 +34,59 @@ function spinWords(string){
   // Using map, we check if each word is 5 characters or longer. If it is, we split the word, reverse the letters, then rejoin them, and finally, rejoin and return the array
   return string.map(word => word.length > 4 ? word.split('').reverse().join('') : word).join(' ');
 }
+
+// ARE THEY THE "SAME"
+//Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that //checks whether the two arrays have the "same" elements, with the same //multiplicities (the multiplicity of a member is the number of times it //appears). "Same" means, here, that the elements in b are the elements in a //squared, regardless of the order.
+//
+//Examples
+//Valid arrays
+//a = [121, 144, 19, 161, 19, 144, 19, 11]  
+//b = [121, 14641, 20736, 361, 25921, 361, 20736, 361]
+//comp(a, b) returns true because in b 121 is the square of 11, 14641 is the //square of 121, 20736 the square of 144, 361 the square of 19, 25921 the square //of 161, and so on. It gets obvious if we write b's elements in terms of squares:
+//
+//a = [121, 144, 19, 161, 19, 144, 19, 11] 
+//b = [11*11, 121*121, 144*144, 19*19, 161*161, 19*19, 144*144, 19*19]
+//Invalid arrays
+//If, for example, we change the first number to something else, comp is not //returning true anymore:
+//
+//a = [121, 144, 19, 161, 19, 144, 19, 11]  
+//b = [132, 14641, 20736, 361, 25921, 361, 20736, 361]
+//comp(a,b) returns false because in b 132 is not the square of any number of a.
+//
+//a = [121, 144, 19, 161, 19, 144, 19, 11]  
+//b = [121, 14641, 20736, 36100, 25921, 361, 20736, 361]
+//comp(a,b) returns false because in b 36100 is not the square of any number of a.
+//
+//Remarks
+//a or b might be [] or {} (all languages except R, Shell).
+//a or b might be nil or null or None or nothing (except in C++, COBOL, Crystal, //D, Dart, Elixir, Fortran, F#, Haskell, Nim, OCaml, Pascal, Perl, PowerShell, //Prolog, PureScript, R, Racket, Rust, Shell, Swift).
+//If a or b are nil (or null or None, depending on the language), the problem //doesn't make sense so return false.
+//
+//Note for C
+//The two arrays have the same size (> 0) given as parameter in function comp.
+
+function comp(array1, array2){
+  // Let's loop through array1 using a for loop, indexing in to each number using array1[i];
+    // In each iteration, let's loop through the entire array2 to check if there is a squared unit.
+    // If there IS a squared unit we've spliced out that array2 number so that on the next loop, it doesn't get checked again, as this could add extra scores to our matchScore if there are 'duplicates'
+    // If matchScore is the same as the length of array1, we've got the "same" 
+  
+  if ((array1 === null || array2 === null) || (array1.length !== array2.length)) {
+    return false;
+  }
+  let matchScore = 0;
+  for (let i = 0; i < array1.length; i++){
+    let matchFound = false;
+    for (let j = 0; j < array2.length; j++){
+      if (array2[j] === (array1[i] * array1[i])) {
+        matchFound = true;
+        array2.splice(j, 1);
+        break;
+      }
+    }
+    if (matchFound){
+      matchScore += 1;
+    }
+  }
+  return (matchScore == array1.length); 
+}
