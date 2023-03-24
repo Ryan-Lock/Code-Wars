d9a15497e5c5d0bbf7ed0b01acd887929581e5d2
Ryan <frostcatlover@outlook.com>
2023-03-24 16:06:42 -0700

    Solved 'Remove Exclamation Marks'

--- 8Kyu.js
+++ 8Kyu.js
@@ -63,3 +63,10 @@ function multiTable(number) {
   product = 10 * number
   return multiTable += `10 * ${number} = ${product}`
 }
+
+// REMOVE EXCLAMATION MARKS
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+  return s.split('').filter(char => char !== '!').join('');
+}
