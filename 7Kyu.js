b630da5c71609ef74b99b141de697867396d63f5
Ryan <frostcatlover@outlook.com>
2023-03-25 15:27:59 -0700

    Solved 'Sum of two lowest positive integers'

--- 7Kyu.js
+++ 7Kyu.js
@@ -41,3 +41,11 @@ function descendingOrder(n){
   // Turning n into a string and splitting it to an array to sort it, rejoin it, and turn it back into an int
   return parseInt(n.toString().split('').sort((a,b) => b-a).join(''));
 }
+
+
+//SUM OF TWO LOEST POSITIVE INTEGERS
+//Create a function that returns the sum of the two lowest positive numbers given an array of minimum 4 positive integers. No floats or non-positive integers will be passed.
+
+//For example, when an array is passed like [19, 5, 42, 2, 77], the output should //be 7.
+//
+//[10, 343445353, 3453445, 3453545353453] should return 3453455.
