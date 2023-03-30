71329fde2eeb607739a43dad9db671a0d0061dea
Ryan <frostcatlover@outlook.com>
2023-03-30 11:45:39 -0700

    Solved 'Replace with alphabet position'

--- 6Kyu.js
+++ 6Kyu.js
@@ -136,3 +136,55 @@ function order(words){
     return parseInt(numString); 
   }
 }
+
+
+//REPLACE WITH ALPHABET POSITION
+//Welcome.
+//
+//In this kata you are required to, given a string, //replace every letter with its position in the //alphabet.
+//
+//If anything in the text isn't a letter, ignore it //and don't return it.
+//
+//"a" = 1, "b" = 2, etc.
+//
+//Example
+//alphabetPosition("The sunset sets at twelve o' //clock.")
+//Should return "20 8 5 19 21 14 19 5 20 19 5 20 19 //1 20 20 23 5 12 22 5 15 3 12 15 3 11" ( as a //string )
+
+function alphabetPosition(text) {
+  text = text.toLowerCase().split('');
+ 
+  let alphabet = [..."abcdefghijklmnopqrstuvwxyz"]
+  let alphaPos = {}
+  
+  for (let i = 0; i < alphabet.length; i++){
+    alphaPos[alphabet[i]] = i+1
+  }
+  
+  let result = ``;
+  
+  for (let i = 0; i < text.length; i++){
+    if (text[i].charCodeAt(0) >= 97 && text[i].charCodeAt(0) <= 122){
+      result += `${alphaPos[text[i]]} `
+    }
+  }
+  return result.slice(0, -1);
+}
+//I initially solved using the solution below, but I thought that chaining methods might be less readable to those not as familiar with JS array methods.
+
+//function alphabetPosition(text) {
+//text = text.toLowerCase().split('');
+//
+//let alphabet = [..."abcdefghijklmnopqrstuvwxyz"]
+//let alphaPos = {}
+//
+//for (let i = 0; i < alphabet.length; i++){
+//  alphaPos[alphabet[i]] = i+1
+//}
+//
+//return text.map(x => {
+//  if(x.charCodeAt(0) >= 97 && x.charCodeAt(0) <= 122){
+//    return alphaPos[x]
+//  }
+//}).filter( x => x !== undefined).join(' ');
+//}
