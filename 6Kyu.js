91a96efcd30e89158909e6027c84e3be57e6b80a
Ryan <frostcatlover@outlook.com>
2023-03-31 10:30:55 -0700

    Solved 'Bouncing balls'

--- 6Kyu.js
+++ 6Kyu.js
@@ -188,3 +188,43 @@ function alphabetPosition(text) {
 //  }
 //}).filter( x => x !== undefined).join(' ');
 //}
+
+// BOUNCING BALLS
+//A child is playing with a ball on the nth floor of a tall building. The height of this floor above ground level, h, is known.
+
+//He drops the ball out of the window. The ball //bounces (for example), to two-thirds of its height //(a bounce of 0.66).
+//
+//His mother looks out of a window 1.5 meters from the //ground.
+//
+//How many times will the mother see the ball pass in //front of her window (including when it's falling and //bouncing?
+//
+//Three conditions must be met for a valid experiment:
+//Float parameter "h" in meters must be greater than 0
+//Float parameter "bounce" must be greater than 0 and //less than 1
+//Float parameter "window" must be less than h.
+//If all three conditions above are fulfilled, return //a positive integer, otherwise return -1.
+//
+//Note:
+//The ball can only be seen if the height of the //rebounding ball is strictly greater than the window //parameter.
+//
+//Examples:
+//- h = 3, bounce = 0.66, window = 1.5, result is 3
+//
+//- h = 3, bounce = 1, window = 1.5, result is -1 
+//
+//(Condition 2) not fulfilled).
+
+function bouncingBall(h,  bounce,  window) {
+  // If test params do not meet any of the requirements we're returning -1
+  if (h <= 0 || bounce <= 0 || bounce >= 1 || window >= h) {
+    return -1;
+  }
+  // The ball will be seen 1 + (bounce * 2) times, minus the last time it bounces based on the condition.
+  let bounces = 0;
+  while (h > window) {
+    h *= bounce;
+    bounces ++;
+  }
+  // Since it will run one last bounce, but the ball won't be seen. So, we subtract 1, because ball is still seen on the way down once when it is initially dropped
+  return bounces*2 - 1;
+}
