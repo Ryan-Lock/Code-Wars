3a9c168305d27495ea631b51e9e68c64bd10ecba
Ryan <frostcatlover@outlook.com>
2023-05-01 22:41:30 -0700

    fixed a comment

--- 6Kyu.js
+++ 6Kyu.js
@@ -312,7 +312,7 @@ function findEvenIndex(arr) {
 }
 
 
-
+//FIND THE ODD INT
 //Given an array of integers, find the one that appears an odd number of times.
 
 //There will always be only one integer that appears an odd number of times.
