9f796f1da361b41ebe1585e004b2ee54a7ad79d1
LearnBite <frostcatlover@outlook.com>
2023-05-09 16:35:04 -0700

    Solved 'row weights'

--- 7Kyu.js
+++ 7Kyu.js
@@ -195,3 +195,23 @@ function declareWinner(fighter1, fighter2, firstAttacker) {
 
   return fighter1.health <= 0 ? fighter2.name : fighter1.name;
 }
+
+// ROW WEIGHTS
+// Scenario
+//Several people are standing in a row divided into two teams.
+//The first person goes into team 1, the second goes into team 2, the third goes //into team 1, and so on.
+//
+//Task
+//Given an array of positive integers (the weights of the people), return a new //array/tuple of two integers, where the first one is the total weight of team 1, //and the second one is the total weight of team 2.
+//
+//Notes
+//Array size is at least 1.
+//All numbers will be positive.
+function rowWeights(array){
+  if (array.length === 1) { return [array[0], 0] }
+  else {
+    let left = array.filter((elem, ind) => ind % 2 == 0).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    let right = array.filter((elem, ind) => ind % 2 != 0).reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return [left, right]
+  }
+}
